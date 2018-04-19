93d24dd2ecb5de20eec50d9a40a73d67e67cb306
medrimonia <medrimonia@gmail.com>
2018-04-19 11:27:07 +0200

    Removing tiny-dnn dependency + adding a script to make data preparation easier

---
+++ scripts/prepare_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 <Balls/Goals> <zip_file>"
+    exit -1
+fi
+
+scripts_path=$(dirname $0 )
+feature_name=$1
+zip_file=$2
+
+img_path=Images/${feature_name}
+pos_path=${img_path}/positive
+neg_path=${img_path}/negative
+
+rm -rf $img_path
+unzip $zip_file
+# Extracting positive images
+mkdir $pos_path
+python ${scripts_path}/get_from_json.py ${img_path}/data.json
+# Moving negative images
+mkdir $neg_path
+mv ${img_path}/*.png ${neg_path}
+# Adding noise
+python ${scripts_path}/add_noise.py ${pos_path}
+python ${scripts_path}/add_noise.py ${neg_path}
