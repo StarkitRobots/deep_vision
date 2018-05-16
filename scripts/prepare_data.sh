e9f874d02c5adfc7c1570e4bac446fa617eea3fc
medrimonia <medrimonia@gmail.com>
2018-05-16 15:32:33 +0200

    Patching scripts in deep vision

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -6,6 +6,7 @@ then
     exit -1
 fi
 
+# Reading arguments
 scripts_path=$(dirname $0 )
 feature_name=$1
 zip_file=$2
@@ -14,8 +15,10 @@ img_path=Images/${feature_name}
 pos_path=${img_path}/positive/
 neg_path=${img_path}/negative/
 
+# Extract data to given folder
 rm -rf $img_path
-unzip $zip_file
+mkdir -p ${img_path}
+unzip $zip_file -d ${img_path}
 # Extracting positive images
 mkdir $pos_path
 python ${scripts_path}/get_from_json.py ${img_path}/data.json
